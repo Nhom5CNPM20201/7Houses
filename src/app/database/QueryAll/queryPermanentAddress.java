ded910c7950cac42cb7922ee5a9dd546ee75095d
Phan Thi Le Hang <ptlh2910@users.noreply.github.com>
2020-11-12 00:04:35 +0700

    Address

--- src/app/database/QueryAll/queryPermanentAddress.java
+++ src/app/database/QueryAll/queryPermanentAddress.java
@@ -1,15 +1,15 @@
 package app.database.QueryAll;
 
 import app.entity.permanentAddress;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class queryPermanentAddress {
+    //truyvan csdl
     public static List<permanentAddress> selectPermanentAddress(Connection connection) throws SQLException {
         List<permanentAddress> list = new ArrayList<>();
         Statement statement = connection.createStatement();
@@ -27,5 +27,58 @@ public class queryPermanentAddress {
         }
         return list;
     }
+    //them dc
+    public static void addNew(Connection conn, permanentAddress permanentAddress)throws  Exception {
+        try {
+            String query = "INSERT INTO DiaChiTT(id, sonha, duongpho, phuong, quan, thanhpho)"
+                    + " values (?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, permanentAddress.getId());
+            preparedStatement.setInt(2, permanentAddress.getNumberHouse());
+            preparedStatement.setString(3, permanentAddress.getStreet());
+            preparedStatement.setString(4, permanentAddress.getSubDistrict());
+            preparedStatement.setString(5, permanentAddress.getDistrict());
+            preparedStatement.setString(6, permanentAddress.getCity());
+            preparedStatement.executeUpdate();
+            ResultSet rs = preparedStatement.getGeneratedKeys();
 
+            if (rs.next()) {
+                int insertedAccountID = rs.getInt(1);
+                permanentAddress.setId(insertedAccountID);
+            }
+            preparedStatement.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //suadiachi
+    public static void updateAddress(Connection connection ,permanentAddress permanentAddress) throws Exception{
+            String query = "Update DiaChiTT set sonha = ? , duongpho = ?, phuong = ?, quan = ?, thanhpho = ? where id = "+ permanentAddress.getId();
+            try{
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setInt(1, permanentAddress.getNumberHouse());
+                preparedStatement.setString(2, permanentAddress.getStreet());
+                preparedStatement.setString(3, permanentAddress.getSubDistrict());
+                preparedStatement.setString(4, permanentAddress.getDistrict());
+                preparedStatement.setString(5, permanentAddress.getCity());
+                preparedStatement.executeUpdate();
+            preparedStatement.close();
+            }
+        catch (SQLServerException e){
+        }
+    }
+        //xoa dia chi
+        public static void remoteAddress(Connection connection ,int id) throws Exception{
+            try {
+                String query = "Delete from DiaChiTT where id = "+ id;
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.executeUpdate();
+                preparedStatement.close();
+            }
+            catch (SQLException e){
+                System.out.println(e.getMessage());
+
+            }
+        }
 }
--- src/app/testConnection.java
+++ src/app/testConnection.java
@@ -1,15 +1,16 @@
 package app;
 
 import app.database.QueryAll.queryAccount;
+import app.database.QueryAll.queryPermanentAddress;
 import app.entity.Account;
+import app.entity.permanentAddress;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 public class testConnection {
-        public static void main(String[] args) throws SQLException {
+        public static void main(String[] args) throws Exception {
             SQLServerDataSource ds = new SQLServerDataSource();
             ds.setUser("sa");
             ds.setPassword("123456");
@@ -19,6 +20,10 @@ public class testConnection {
             Connection connection = ds.getConnection();
             List<Account> list = queryAccount.selectAccount(connection);
             System.out.println(list.get(0).getName());
+            queryPermanentAddress.addNew(connection,new permanentAddress(1,4,"hh", "gg" , "aa", "kk"));
+            queryPermanentAddress.updateAddress(connection,new permanentAddress(1,5,
+                    "hh", "gg" , "aa", "kk" ));
+            queryPermanentAddress.remoteAddress(connection, 1);
     }
 
 }
