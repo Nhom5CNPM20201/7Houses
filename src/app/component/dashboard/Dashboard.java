2ccf829e105080280e7ace0920bcf474aa74689f
DESKTOP-3JUQEMR\Tunsss^^ <tuantoquq@gmail.com>
2020-11-14 16:14:59 +0700

    update dashboard, search household,people UI

--- src/app/component/dashboard/Dashboard.java
+++ src/app/component/dashboard/Dashboard.java
@@ -1,4 +1,80 @@
 package app.component.dashboard;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class Dashboard {
+
+    @FXML
+    private AnchorPane mainView;
+
+    @FXML
+    private Button btnLogOut;
+
+    @FXML
+    private Button btnDashborad;
+
+    @FXML
+    private Button btnHouseHold;
+
+    @FXML
+    private Button btnPeople;
+
+    @FXML
+    private Button btnFee;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Button btnStatistic;
+
+    @FXML
+    private Button btnInfomation;
+
+    @FXML
+    private AnchorPane homePane;
+
+    @FXML
+    void dashboardOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void feeOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void houseHoldOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void informationOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOutOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void peopleOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void statisticOnClick(ActionEvent event) {
+
+    }
+
 }
---
+++ src/app/component/dashboard/search/Search.java
@@ -0,0 +1,25 @@
+package app.component.dashboard.search;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class Search {
+
+    @FXML
+    private ComboBox<?> comboboxOption;
+
+    @FXML
+    private Button btnFind;
+
+    @FXML
+    void findOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void optionOnAction(ActionEvent event) {
+
+    }
+}
