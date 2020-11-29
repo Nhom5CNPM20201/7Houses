e4c4697661203e2f4a89ae5cf306dd34e8f4089e
hanichan2000 <64351493+hanichan2000@users.noreply.github.com>
2020-11-29 11:05:15 +0700

    update form address

---
+++ src/app/component/common/AddressForm.java
@@ -0,0 +1,42 @@
+package app.component.common;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddressForm {
+
+    @FXML
+    private TextField txtHouseHoldNo;
+
+    @FXML
+    private TextField txtStreet;
+
+    @FXML
+    private TextField txtWard;
+
+    @FXML
+    private TextField txtDistrict;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private Button btnOK;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    void onClickCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickOK(ActionEvent event) {
+
+    }
+
+}
+
