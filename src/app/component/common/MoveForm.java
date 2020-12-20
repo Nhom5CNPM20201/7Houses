93fc75bcb54554ebbed0eba2458c0c7c03a59ec4
DESKTOP-3JUQEMR\Tunsss^^ <tuantoquq@gmail.com>
2020-12-20 12:16:36 +0700

    update moveform

--- src/app/component/common/MoveForm.java
+++ src/app/component/common/MoveForm.java
@@ -1,16 +1,23 @@
 package app.component.common;
 
+import app.entity.Address;
+import app.utility.viewUtils.Mediator;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,10 +29,17 @@ public class MoveForm implements Initializable {
 
     @FXML
     private Button btnSearch;
+    @FXML
+    private TextField tfAddressDes;
+    @FXML
+    private Button btnAdd;
 
     @FXML
-    private TextField tfAddress;
+    private TextField tfAddressCur;
 
+    private Stage stage;
+
+    private AddressForm addressController;
     @FXML
     private DatePicker dateMove;
 
@@ -40,7 +54,7 @@ public class MoveForm implements Initializable {
 
     @FXML
     private Button btnCancel;
-
+    private Address newAddress;
     @FXML
     void cancelOnclick(ActionEvent event) {
 
@@ -49,6 +63,27 @@ public class MoveForm implements Initializable {
     @FXML
     void okOnclick(ActionEvent event) {
 
+    }
+    @FXML
+    void addOnclick(ActionEvent event) throws IOException {
+        if (this.stage != null) this.stage.close();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AddressForm.fxml"));
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root, 395, 422);
+        stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+        this.addressController = loader.getController();
+    }
+    private void onCloseAddAddress(ActionEvent e) {
+        this.newAddress = this.addressController != null ? this.addressController.getNewAddress() : null;
+
+        if (newAddress != null) {
+            tfAddressDes.setText(newAddress.getDetail());
+        }
+        this.stage.close();
     }
 
     @FXML
@@ -59,5 +94,7 @@ public class MoveForm implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         moveType.setItems(list);
+        Mediator.unSubscribe("onCloseAddAddress");
+        Mediator.subscribe("onCloseAddAddress", event -> onCloseAddAddress(null));
     }
 }
